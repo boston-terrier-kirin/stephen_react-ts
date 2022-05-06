5527286b9c3d8d74747748e1431291a4b0d10fdd
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-06 15:27:48 +0900

    02-redux-ts/Section4-44

--- 02-redux-ts/src/components/RepositoriesList.tsx
+++ 02-redux-ts/src/components/RepositoriesList.tsx
@@ -1,12 +1,16 @@
 import { FormEvent, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { actionCreators } from '../state';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const dispatch = useDispatch();
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -16,12 +20,22 @@ const RepositoriesList: React.FC = () => {
     searchRepositories(term);
   };
 
+  let contents;
+  if (error) {
+    contents = error;
+  } else if (loading) {
+    contents = 'Loading...';
+  } else if (data) {
+    contents = data.map((name) => <div key={name}>{name}</div>);
+  }
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={term} onChange={(event) => setTerm(event.target.value)} />
         <button>Search</button>
       </form>
+      {contents}
     </div>
   );
 };
