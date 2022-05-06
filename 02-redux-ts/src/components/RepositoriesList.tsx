65aa04906b1d49a344a9c6a123f3837c58026339
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-05-06 14:59:53 +0900

    02-redux-ts/Section4-39
    RepositoriesList
    TODO：コンパイルエラーで動かない
    dispatch(actionCreators.searchRepositories(term));

--- 02-redux-ts/src/components/RepositoriesList.tsx
+++ 02-redux-ts/src/components/RepositoriesList.tsx
@@ -1,8 +1,25 @@
+import { FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useActions } from '../hooks/useActions';
+import { actionCreators } from '../state';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // TODO：コンパイルエラーで動かない
+    // dispatch(actionCreators.searchRepositories(term));
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(event) => setTerm(event.target.value)} />
         <button>Search</button>
       </form>
     </div>
